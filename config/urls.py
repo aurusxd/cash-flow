8fafdb0e20a49e1fd264858e910c39a686ccd12b
aurusxd <warface.clan01@gmail.com>
2026-06-26 23:17:55 +0700

    feat: add record_list view

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cashflow.views import record_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', record_list),
 ]
