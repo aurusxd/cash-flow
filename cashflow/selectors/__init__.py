f3fd8e32d24fe5294aeeaecacbcd048020f9b8ae
aurusxd <warface.clan01@gmail.com>
2026-06-27 16:56:58 +0700

    feat: add create view

---
+++ cashflow/selectors/__init__.py
@@ -0,0 +1,3 @@
+from cashflow.selectors.record import get_records_queryset
+
+__all__ = ("get_records_queryset",)
--- cashflow/views/record.py
+++ cashflow/views/record.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
@@ -13,13 +14,36 @@ class RecordListView(ListView):
 
     context_object_name = "records"
 
-    queryset = get_records_queryset()
+    def get_queryset(self):
+        queryset = get_records_queryset()
+
+        if date_from := self.request.GET.get("date_from"):
+            queryset = queryset.filter(date__gte=date_from)
+
+        if date_to := self.request.GET.get("date_to"):
+            queryset = queryset.filter(date__lte=date_to)
+
+        if status := self.request.GET.get("status"):
+            queryset = queryset.filter(status_id=status)
+
+        if operation_type := self.request.GET.get("operation_type"):
+            queryset = queryset.filter(operation_type_id=operation_type)
+
+        if category := self.request.GET.get("category"):
+            queryset = queryset.filter(category_id=category)
+
+        if subcategory := self.request.GET.get("subcategory"):
+            queryset = queryset.filter(subcategory_id=subcategory)
+
+        return queryset
 
 
 class RecordCreateView(CreateView):
     model = Record
     form_class = RecordForm
-
     template_name = "record_form.html"
-
     success_url = reverse_lazy("record_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Запись успешно создана.")
+        return super().form_valid(form)
